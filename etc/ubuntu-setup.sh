84053e75ba565413967a03d7c583bb296483f934
lucidus21 <lucidus21@gmail.com>
2015-04-21 16:43:34 +0900

    Etc: ubuntu setup script (wook)

---
+++ etc/ubuntu-setup.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+# from @wookayin https://github.com/wookayin/dotfiles
+
+install_essential_packages() {
+	local -a packages; packages=( \
+		build-essential \
+		vim zsh curl \
+		python-software-properties software-properties-common \
+		cmake cmake-data \
+		terminator htop \
+		silversearcher-ag \
+		openssh-server mosh \
+		)
+
+	sudo apt-get install -y ${packages[@]}
+}
+
+install_ppa_git() {
+	# https://launchpad.net/~git-core/+archive/ubuntu/ppa
+	sudo add-apt-repository -y ppa:git-core/ppa
+	sudo apt-get update
+	sudo apt-get install -y git-all
+}
+
+install_ppa_tmux() {
+	# https://launchpad.net/~pi-rho/+archive/ubuntu/dev
+	sudo add-apt-repository -y ppa:pi-rho/dev
+	sudo apt-get update
+	sudo apt-get install -y tmux
+}
+
+
+# entrypoint script
+if [ `uname` != "Linux" ]; then
+	echo "Run on Linux (not on Mac OS X)"; exit 1
+fi
+if [ -n "$1" ]; then
+	$1
+else
+	echo "Usage: $0 [command], where command is one of the following:"
+	declare -F | cut -d" " -f3
+fi
